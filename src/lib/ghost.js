bd4b0bf5c76c59c32fde8312ae0e2233e8c571f0
Ahmad Farooq <petemaverickk@gmail.com>
2026-02-10 20:07:13 +0500

    Update ghost.js

--- src/lib/ghost.js
+++ src/lib/ghost.js
@@ -1,9 +1,9 @@
 import GhostContentAPI from "@tryghost/content-api";
 
 const ghost = new GhostContentAPI({
-  url: import.meta.env.VITE_GHOST_API_URL,
-  key: import.meta.env.VITE_GHOST_CONTENT_KEY,
-  version: "v5.0"
+  url: "https://ahmad-farooq.ghost.io",
+  key: "145bdada9a070654a86808da2e",
+  version: "v5.0",
 });
 
-export default ghost;
+export { ghost };
